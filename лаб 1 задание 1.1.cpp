6e646c68dc27c65e972e6ba3ae40432fd51b378e
Гриша Васильев <gregory.vasiliev@gmail.com>
2024-02-24 00:39:11 +0300

    Добавьте файлы проекта.

---
+++ лаб 1 задание 1.1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+    stack<double> myStack;
+    setlocale(LC_ALL, "RU");
+    myStack.push(3.14);
+    myStack.push(2.71);
+    myStack.push(5.55);
+    myStack.push(4.99);
+
+    // Определение максимального элемента в стеке
+    double maxElement = myStack.top();
+    stack<double> tempStack;
+
+    while (!myStack.empty()) {
+        if (myStack.top() > maxElement) {
+            maxElement = myStack.top();
+        }
+        tempStack.push(myStack.top());
+        myStack.pop();
+    }
+
+    // Восстановление исходного стека
+    while (!tempStack.empty()) {
+        myStack.push(tempStack.top());
+        tempStack.pop();
+    }
+
+    // Вывод максимального элемента
+    cout << "Максимальный элемент в стеке: " << maxElement << std::endl;
+
+    // Просмотр данных стека
+    cout << "Данные в стеке: ";
+    while (!myStack.empty()) {
+        cout << myStack.top() << " ";
+        myStack.pop();
+    }
+    cout << std::endl;
+
+    return 0;
+}
